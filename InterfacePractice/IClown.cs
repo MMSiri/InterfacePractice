d3a9909e823fc7afe78c6715c4b7d33e3faf67fd
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-08-11 18:53:12 +0100

    Add project files.

---
+++ InterfacePractice/IClown.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfacePractice
+{
+    interface IClown
+    {
+        string FunnyThingIHave { get; }
+
+        void Honk();
+    }
+}
---
+++ InterfacePractice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InterfacePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TallGuy tallGuy = new TallGuy() { Name = "Jimmy", Height = 76 };
+            tallGuy.TalkAboutYourself();
+            Console.WriteLine($"The tall guy has {tallGuy.FunnyThingIHave}");
+            tallGuy.Honk();
+        }
+    }
+}
